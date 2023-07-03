2c408104d8930c1e31148b25771e663a5dfa8890
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-07-03 14:06:34 +0530

    locationAdded

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Food RS 10 </h2>
+      </div>
+
+      <div>
+        <h2>Petrol RS 100 </h2>
+      </div>
+
+      <div>
+        <h2>Movies RS 200 </h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
