a9747b084ac13db8ceef9a8445a9355882616be5
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-07-12 10:17:05 +0530

    ConditionalRendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expense.css";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -14,6 +14,24 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+  //  let expensesContent;
+
+  // if (filteredExpenses.length === 0) {
+  //   expensesContent = <p>No expenses found.</p>;
+  // } else if (filteredExpenses.length === 1) {
+  //   expensesContent = <p>Only one expense found.</p>;
+  // } else {
+  //   expensesContent = filteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
+  
+  
 
   return (
     <div>
@@ -22,18 +40,17 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 0 ? (
-          <p>No expenses found for the selected year.</p>
-        ) : (
-          filteredExpenses.map((expense) => (
+        <ExpensesList items={filteredExpenses}/>
+       
+          {/* {filteredExpenses.map((expense) => (
             <ExpenseItem
               key={expense.id}
               title={expense.title}
               amount={expense.amount}
               date={expense.date}
             />
-          ))
-        )}
+          ))} */}
+        
 
         {/* <ExpenseItem
           title={props.items[0].title}
